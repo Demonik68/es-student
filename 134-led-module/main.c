b1a77859c297607f337c09683cb9118203f6b4b8
Demonik68 <dima.rulev.93@gmail.com>
2026-09-13 15:01:39 +0200

    Сделана п1.3.6

--- 134-led-module/main.c
+++ 134-led-module/main.c
@@ -2,6 +2,7 @@
 #include "hardware/gpio.h"
 #include <stdio.h>
 #include "led.h"
+#include "log.h"
 
 
 const uint BUTTON_PIN = 15;
@@ -20,16 +21,20 @@ void handle_command(int command)
     if (command == 'e')
     {
         led_set(true);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
     }
     else if (command == 'd')
     {
         led_set(false);
-        printf("led %s\n", led_is_on() ? "on" : "off");
+        LOG_INF("led %s\n", led_is_on() ? "on" : "off");
+    }
+    else if (command == 'v')
+    {
+        log_version();
     }
     else
     {
-        printf("unknown command: %c\n", command);
+        LOG_ERR("unknown command: %c\n", command);
     }
 }
 
@@ -53,7 +58,7 @@ int main()
         if (previous == true && current == false)
         {
             led_toggle();
-            printf("led %s\n", led_is_on() ? "on" : "off");
+            LOG_INF("led %s\n", led_is_on() ? "on" : "off");
         }
 
         previous = current;
@@ -64,7 +69,8 @@ int main()
         {
             continue;
         }
-
+        
+        LOG_DBG("got %c\n", command);
         handle_command(command);
     }
 }
